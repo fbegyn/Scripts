0a3d10d81c0322dd4e02b665c4d2b4c7c59d4de1
Francis Begyn <francis@begyn.be>
2022-06-20 12:52:01 +0200

    youtubeplayer - fix queueing scripts

--- youtubeplayer.sh
+++ youtubeplayer.sh
@@ -1,23 +1,35 @@
 #! /usr/bin/env bash
-QUEUEFILE=$HOME/Videos/.mpvinput
-if [[ ! -p $QUEUEFILE ]]; then
-    mkfifo $QUEUEFILE
-fi
 
 case $1 in
-  "queue" )
+    # queue means opening up a video player and adding videos to a fifo queue
+    "queue" )
+    QUEUEFILE=$HOME/Videos/.mpvinput
+    # check if there already it a yt-player proces running
     if pgrep -f yt-player > /dev/null
     then
-      notify-send "Adding video to mpv queue" "Adding $2 to the video queue"
-      printf "%s\n" "loadfile \"$2\" append-play" > $QUEUEFILE
+		# if so, add the new url to the fifo queue
+		notify-send "Adding video to mpv queue" "Adding $2 to the video queue"
+		resp=$(
+		  echo "{\"command\":[\"loadfile\",\"$2\",\"append-play\"]}" \
+			  | socat - $QUEUEFILE \
+			  | jq -r '.error, .data.playlist_entry_id'
+		)
+		notify-send "Video added to playlist, status" "$resp"
     else
-      notify-send "New MPV player" "No player is open, starting a new player"
-      mpv --no-terminal --x11-name='yt-player' --input-file=$QUEUEFILE --player-operation-mode=pseudo-gui "$2" &
+        notify-send "New MPV player" "No player is open, starting a new player"
+        mpv --no-terminal \
+			--x11-name='yt-player' \
+			--input-ipc-server=$QUEUEFILE \
+			--player-operation-mode=pseudo-gui "$2"
+
+	    rm $QUEUEFILE
     fi
     ;;
-  "window" )
+    "window" )
     notify-send "New MPV player" "Opening a new player window"
-    mpv --no-terminal --x11-name='video-player' --input-file=$QUEUEFILE --player-operation-mode=pseudo-gui "$2" &
+    mpv --no-terminal \
+		--x11-name='video-player' \
+		--player-operation-mode=pseudo-gui "$2" &
     ;;
 esac
 
